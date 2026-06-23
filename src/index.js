926f3bb4c39d095171f269e045750422b7751fc2
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-06-23 16:34:49 +0200

    feat: add Form component export and bump package version to 0.0.4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { CookieConsentDialog, CookieConsentForm } from './cookieconsent/index.js'
+import { Form } from './form/index.js'
 import { GoogleMaps } from './google-maps/index.js'
 import { Popover } from './popover/index.js'
 import { ReCaptcha } from './recaptcha/index.js'
 import { Reveal } from './reveal/index.js'
 
-export { CookieConsentDialog, CookieConsentForm, GoogleMaps, Popover, ReCaptcha, Reveal }
+export { CookieConsentDialog, CookieConsentForm, GoogleMaps, Popover, ReCaptcha, Reveal, Form }
