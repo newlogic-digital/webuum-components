9976a29fc2fca7c80e89d74882f22d93eba79ec5
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-06-25 13:38:03 +0200

    fix: add Header component to exports in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { CookieConsentDialog, CookieConsentForm } from './cookieconsent/index.js'
 import { Form } from './form/index.js'
 import { GoogleMaps } from './google-maps/index.js'
+import { Header } from './header/index.js'
 import { Popover } from './popover/index.js'
 
-export { CookieConsentDialog, CookieConsentForm, GoogleMaps, Popover, Form }
+export { CookieConsentDialog, CookieConsentForm, GoogleMaps, Header, Popover, Form }
